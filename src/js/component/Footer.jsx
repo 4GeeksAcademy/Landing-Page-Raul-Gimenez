d22db02b75e459c85f3a2cd3bfbbd60986ce2c99
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-08-02 22:46:47 +0000

    first commit

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+ export const Footer = () => {
+
+	return (<footer>
+        <div className="container">
+        Copyright @ Bíceps y rizos 2023
+        </div>
+      </footer>
+		
+	);
+};
+
