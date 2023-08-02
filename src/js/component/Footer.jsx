624afca03b5c7cd29691052eb01afe2da6ea6ab5
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-08-02 22:50:08 +0000

    formatted

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -1,12 +1,8 @@
 import React from "react";
- export const Footer = () => {
-
-	return (<footer>
-        <div className="container">
-        Copyright @ Bíceps y rizos 2023
-        </div>
-      </footer>
-		
-	);
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="container">Copyright @ Bíceps y rizos 2023</div>
+    </footer>
+  );
 };
-
